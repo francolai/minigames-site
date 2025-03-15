e4d77213ff4fe526dc112d49e1aefee133338189
Franco Lai <francolai2012@gmail.com>
2025-03-16 04:08:46 +0800

    fix code for gh-pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter } from 'react-router';
+import { createHashRouter } from 'react-router';
 import { RouterProvider } from 'react-router';
 
 import RootPage from './components/root-page/RootPage.jsx';
 import TicTacToe from './components/tic-tac-toe/TicTacToe.jsx';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <RootPage />,
