810664291cd93be0f00c036dfe264dffebf9c4ce
Franco Lai <francolai2012@gmail.com>
2025-05-03 20:47:55 +0800

    change config for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteSingleFile } from 'vite-plugin-singlefile';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/minigames-site/',
-  plugins: [react(), viteSingleFile()],
+  plugins: [react()],
 });
